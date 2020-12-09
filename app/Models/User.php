7f42ffe5361b69f8f98298a012e8179a971c4db4
Nailul Firdaus <nail.nafir@gmail.com>
2020-12-09 17:54:48 +0700

    fix error hash password

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 use Carbon\Carbon;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Jetstream\HasTeams;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Jetstream\HasProfilePhoto;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
@@ -69,4 +70,11 @@ class User extends Authenticatable
     {
         return Carbon::parse($value)->timestamp;
     }
+
+    public function setPasswordAttribute($password)
+    {
+        if ( !empty($password) ) {
+    		$this->attributes['password'] = Hash::make($password);
+		}
+   	 }
 }
